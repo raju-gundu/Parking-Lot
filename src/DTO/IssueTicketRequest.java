f7b24fb6814fd4b8c23d8491d89621ec2c5aed35
raju <rajugundu849@gmail.com>
2024-06-20 01:03:18 +0530

    Created DTO

---
+++ src/DTO/IssueTicketRequest.java
@@ -0,0 +1,42 @@
+package DTO;
+
+import Models.VehicleType;
+
+public class IssueTicketRequest {
+    private VehicleType vehicleType;
+    private String vehicleNumber;
+    private String vehicleOwner;
+    private Long gateId;
+
+    public VehicleType getVehicleType() {
+        return vehicleType;
+    }
+
+    public void setVehicleType(VehicleType vehicleType) {
+        this.vehicleType = vehicleType;
+    }
+
+    public String getVehicleNumber() {
+        return vehicleNumber;
+    }
+
+    public void setVehicleNumber(String vehicleNumber) {
+        this.vehicleNumber = vehicleNumber;
+    }
+
+    public String getVehicleOwner() {
+        return vehicleOwner;
+    }
+
+    public void setVehicleOwner(String vehicleOwner) {
+        this.vehicleOwner = vehicleOwner;
+    }
+
+    public Long getGateId() {
+        return gateId;
+    }
+
+    public void setGateId(Long gateId) {
+        this.gateId = gateId;
+    }
+}
